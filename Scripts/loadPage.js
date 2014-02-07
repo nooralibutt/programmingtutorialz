de0f5633a4a62a5867f730ee5252f3deaf926cf2
nooralibutt <nooralibutt@gmail.com>
2014-02-07 21:17:12 +0500

    Revert "last Please"
    This reverts commit 743ae428c6c8e539eb40f72cfac260e65d05c4d6.

--- Scripts/loadPage.js
+++
@@ -1,7 +0,0 @@
-﻿$(document).ready(function () {
-    $('#left-nav a').click(function (e) {
-        e.preventDefault();
-        var v = e.target.getAttribute('href');
-        $('#left-nav').text = v;
-    });
-});
