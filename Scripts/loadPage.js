725a0e84d05547745810d6c183aac583bbaf4a3d
nooralibutt <nooralibutt@gmail.com>
2014-02-07 21:29:18 +0500

    please

---
+++ Scripts/loadPage.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function () {
+    $('#left-nav a').click(function (e) {
+        e.preventDefault();
+        var v = e.target.getAttribute('href');
+        $('#left-nav').text = v;
+    });
+});
